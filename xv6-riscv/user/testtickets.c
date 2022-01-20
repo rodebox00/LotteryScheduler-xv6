7fb760bcafe9b704407e3f3aab8fb449a28d10ba
Rodrigo <79221711+rodebox00@users.noreply.github.com>
2022-01-20 14:13:25 +0100

    xv6 upload

---
+++ xv6-riscv/user/testtickets.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+int
+main(int argc, char *argv[])
+{
+  int tickets = 5;
+  if (settickets(tickets)!= 0)
+  {
+    printf("Sys call error: %d\n", tickets);
+  }else printf("Successfull sys call...\n"); 
+  exit(0);
+}
